9355012be066c190d9d630a15c3d1c95ac10c920
betsystevens <betsystevens5@gmail.com>
2021-03-16 23:12:48 -0500

    change PDO fetch to return only associative array (not indexed too)

--- DatabaseFunctions.php
+++ DatabaseFunctions.php
@@ -9,6 +9,5 @@ function getAllOrderBy($pdo, $table, $orderBy){
 	$sql = 'SELECT * FROM `' .$table.
 						'` ORDER BY `' .$orderBy. '`';
 	$result = query($pdo, $sql);
-	// fetchAll() returns an array of all records retrieved
-	return $result->fetchAll();	
+	return $result->fetchAll(PDO::FETCH_ASSOC);
 }
